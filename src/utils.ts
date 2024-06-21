8661ca44c750c71f2133d9ad428aaea95a2199ca
Jimmy <jimmyrss1102@gmail.com>
2024-06-22 03:35:03 +0800

    打印

--- src/utils.ts
+++ src/utils.ts
@@ -97,7 +97,9 @@ export const generateViteTemplate = async ({ rootDir, currentDir }: { rootDir: s
       fs.writeFileSync(viteConfigFile, newViteConfig);
 
       const templateDir = `${currentDir}/${templateName[0]}`;
+      core.info(`进入前的目录: ${process.cwd()}`);
       process.chdir(templateDir);
+      core.info(`进入后的目录: ${process.cwd()}`);
 
       exec(`pnpm install && pnpm run build`);
 
